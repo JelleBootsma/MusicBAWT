5bd103d905b219dcfe856d88e3bee63ae3eb2292
JelleBootsma <jellehb@gmail.com>
2020-03-14 18:42:07 +0100

    Template project

---
+++ MusicBAWT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicBAWT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
